8de274429ba240d7da58fcf9fde4804ddc319be6
Michael Lowry <michael.lowry64@gmail.com>
2024-04-17 10:37:09 -0400

    Completed 02_repeatString

--- 02_repeatString/repeatString.js
+++ 02_repeatString/repeatString.js
@@ -1,5 +1,12 @@
-const repeatString = function() {
+const repeatString = function(s, n) {
+    let newString = ""
 
+    if (n < 0) return "ERROR";
+
+    for (let i = 0; i < n; i++)
+        newString = newString + s;
+
+    return newString;
 };
 
 // Do not edit below this line
