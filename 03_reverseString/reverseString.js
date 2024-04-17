be32539db2ec95801275045d2290292d22a4506d
Michael Lowry <michael.lowry64@gmail.com>
2024-04-17 10:42:57 -0400

    Completed 03_reverseString

--- 03_reverseString/reverseString.js
+++ 03_reverseString/reverseString.js
@@ -1,5 +1,10 @@
-const reverseString = function() {
-
+const reverseString = function(s) {
+    let reverseString = '';
+    
+    for (let i = s.length - 1; i >= 0; i--)
+        reverseString = reverseString + s[i];
+    
+    return reverseString;
 };
 
 // Do not edit below this line
