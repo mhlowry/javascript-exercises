7362776487fde6b77540dea175c985bd52b28458
Michael Lowry <michael.lowry64@gmail.com>
2024-04-19 08:40:22 -0400

    Completed 04_removeFromArray

--- 04_removeFromArray/removeFromArray.js
+++ 04_removeFromArray/removeFromArray.js
@@ -1,5 +1,19 @@
 const removeFromArray = function() {
 
+    if (arguments.length < 2){
+        console.error("Not enough arguments");
+        return;
+    }
+
+    let arr = arguments[0];
+
+    for(let i = 1; i < arguments.length; i++){
+
+        while(arr.indexOf(arguments[i]) != -1)
+            arr.splice(arr.indexOf(arguments[i]), 1);
+    }
+
+    return arr;
 };
 
 // Do not edit below this line
